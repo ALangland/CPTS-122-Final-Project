bae01e92bc7c454d7ac4ba669420c278426d7c71
Ethan Burzynski <ethanburz@gmail.com>
2023-04-26 11:32:01 -0700

    button class mostly finished, needs a better game loop in the future - probably some class that wraps the whole project up and stuff. also probably need to add functionality to reset the button to not being pressed

--- PA9/PA9/main.cpp
+++ PA9/PA9/main.cpp
@@ -1,9 +1,11 @@
 #include <SFML/Graphics.hpp>
+#include "Button.hpp"
 
 int main(void) {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Title");
-
+	Button mButton(50, 50, 100, 40, "EPIC BUTTON", sf::Color(70,70,70,200), sf::Color(150, 150, 150, 255), sf::Color(20, 20, 20, 200), true);
 	sf::Event event;
+	sf::Vector2i mousePos;
 
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
@@ -11,6 +13,11 @@ int main(void) {
 				window.close();
 			}
 		}
+		window.clear();
+		mousePos = sf::Mouse::getPosition(window);
+		mButton.updateButton((sf::Vector2f)mousePos);
+		mButton.drawbutton(window);
+		window.display();
 	}
 
 	return 0;
