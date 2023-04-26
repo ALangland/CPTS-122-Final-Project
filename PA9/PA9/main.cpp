7265fab41d9eb97c3cd5275c66ca51304cb90eec
ALangland <alxlangland@gmail.com>
2023-04-25 20:18:30 -0700

    SFML DLLs installed

--- PA9/PA9/main.cpp
+++ PA9/PA9/main.cpp
@@ -1,3 +1,17 @@
-int main(void) {
+#include <SFML/Graphics.hpp>
 
+int main(void) {
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Title");
+
+	sf::Event event;
+
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+	}
+
+	return 0;
 }
